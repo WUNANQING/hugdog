abfcf73dd3c586a0b5222a1df1f2a01f52d5feb9
lopthick <lopthick3@gmail.com>
2020-03-11 19:30:43 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/event/Home'
+import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Blog from './pages/BLOG/Blog'
