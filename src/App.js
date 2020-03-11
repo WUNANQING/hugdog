5405a8930141b2314a7efb84069d8f1c2e38ddb3
lopthick <lopthick3@gmail.com>
2020-03-11 19:08:47 +0800

    event create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/event/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
