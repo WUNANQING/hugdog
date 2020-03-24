b96ba93eef357de670386b0ce09ea283a49ff26e
eloise <55773031+bananajumpjump@users.noreply.github.com>
2020-03-24 17:29:52 +0800

    update question page

--- src/pages/Knowledge/Question.js
+++ src/pages/Knowledge/Question.js
@@ -1,76 +1,156 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import {
   Container,
+  Collapse,
   Row,
   Col,
-  Accordion,
   Card,
+  Form,
   Button,
-  Badge,
+  Modal,
   Image,
 } from 'react-bootstrap'
+import { MdPets } from 'react-icons/md'
+//redux
+import { connect } from 'react-redux'
+//action
+import { bindActionCreators } from 'redux'
+import { getQuestion } from './actions/index'
+
+import $ from 'jquery'
 
 import Swal from 'sweetalert2'
 
 import '../../components/Knowledge/knowledge.scss'
 import QuestionArt from './QuestionArt'
 
-function Question() {
+function Question(props) {
+  useEffect(() => {
+    console.log(props)
+    props.getQuestion()
+  }, [])
+
+  // useEffect(() => {
+  //   props.getDogData()
+  // }, [])
+
   const Swal = require('sweetalert2')
 
   function post() {
-    Swal.mixin({
-      input: 'text',
-      confirmButtonText: '確定',
-      cancelButtonText: '取消',
-      showCancelButton: true,
-      progressSteps: ['發問'],
+    Swal.fire({
+      icon: 'success',
+      title: '發問成功',
     })
-      .queue([
-        {
-          title: '我要發問',
-          text: '請輸入您的問題',
-        },
-      ])
-      .then(result => {
-        if (result.value) {
-          const answers = JSON.stringify(result.value)
-          Swal.fire({
-            title: '發問成功',
-            icon: 'success',
-          })
-          // Swal.fire({
-          //   title: 'All done!',
-          //   html: `
-          //   Your answers:
-          //   <pre><code>${answers}</code></pre>
-          // `,
-          //   confirmButtonText: 'Lovely!',
-          // })
-        }
-      })
   }
+
+  const [show, setShow] = useState(false)
+  const handleClose = () => {
+    setShow(false)
+    // setTimeout(() => {
+    post()
+    // }, 100)
+  }
+  const handleShow = () => setShow(true)
+
   return (
     <>
       <div className="knowledgebanner"></div>
       <Container className="question">
         <div>
-          <Button
+          {/* <Button
             className="askquestion"
             variant="warning"
             onClick={() => post()}
           >
             我要發問
+          </Button> */}
+          <Button
+            className="askquestion"
+            variant="primary"
+            onClick={handleShow}
+            aria-controls="example-collapse-text"
+            // aria-expanded={open}
+          >
+            我要發問
           </Button>
+
+          <Modal
+            show={show}
+            onHide={handleClose}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>
+                <MdPets /> 我要發問
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form>
+                  <Form.Group controlId="exampleForm.ControlSelect1 petselect">
+                    {/* <Form.Label>寵物</Form.Label> */}
+                    <Form.Control as="select">
+                      <option>請選擇寵物</option>
+                      <option>1</option>
+                      {/* <option>{props.data.qName}</option> */}
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlSelect2 typeselect">
+                    <Form.Control as="select">
+                      <option>請選擇問題類別</option>
+                      <option>行為</option>
+                      <option>照護</option>
+                      <option>飲食</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Control type="email" placeholder="請輸入問題標題" />
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Control
+                      as="textarea"
+                      rows="8"
+                      placeholder="請詳述說明狀況、發生時間、主要徵狀、寵物變化..."
+                    />
+                  </Form.Group>
+                </Form>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                取消發問
+              </Button>
+              <Button variant="primary" onClick={handleClose}>
+                發佈
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
-        <Row>
-          <Col>
-            <QuestionArt />
-          </Col>
-        </Row>
+        <div>
+          <Row>
+            <Col xs={12} md={12}>
+              <Row xs={12} md={6}>
+                {props.post &&
+                  props.post.map((value, index) => {
+                    return <QuestionArt key={index} data={props.post[index]} />
+                  })}
+              </Row>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </>
   )
 }
 
-export default Question
+const mapStateToProps = store => {
+  return { post: store.getQuestion }
+}
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ getQuestion }, dispatch)
+}
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(Question)
+)
--- src/pages/Knowledge/QuestionArt.js
+++ src/pages/Knowledge/QuestionArt.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { withRouter, Link } from 'react-router-dom'
 import {
   Container,
   Row,
@@ -13,131 +14,77 @@ import {
 
 // const [open, setOpen] = useState(false)
 
-function QuestionArt() {
+function QuestionArt(props) {
+  let showQuestionType = ''
+  if (props.data.qType === '1') {
+    showQuestionType = '食慾/精神不濟'
+  } else if (props.data.qType === '2') {
+    showQuestionType = '排泄異常'
+  } else if (props.data.qType === '3') {
+    showQuestionType = '營養與處方'
+  } else {
+    showQuestionType = '其他'
+  }
+
   return (
     <>
-      <Container>
-        <Row>
-          <Col md={6}>
-            <Accordion className="Card">
-              <Card className="">
-                <Card.Header className="bgColor ">
-                  <Card.Title as="h4" className="d-flex">
-                    <Figure>
-                      <Image
-                        src="http://fakeimg.pl/50x50/ff73b3/EAE0D0/"
-                        // roundedCircle
-                      />
-                    </Figure>
-                    <div> 關於每年施打疫苗，是必須的嗎？ </div>
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>分類</Card.Subtitle>
+      <Accordion className="Card m-2" style={{ width: '33rem' }}>
+        <Card className="">
+          <Card.Header
+            className="light"
+            // style={{ background: 'orange', }}
+          >
+            <Card.Title as="h4" className="d-flex">
+              <Figure className="d-flex align-items-start flex-column bd-highlight mb-3">
+                <Image
+                  className="questionImg"
+                  src={require('../../images/knowledge/question/' +
+                    props.data.qId +
+                    '.jpg')}
+                  alt=""
+                  roundedCircle
+                />
+                <Card.Subtitle className="align-self-center mt-3">
+                  <strong> {props.data.mName}</strong>
+                </Card.Subtitle>
+              </Figure>
+              <div className="align-self-center ml-2">{props.data.qTitle}</div>
+              <Badge>{showQuestionType}</Badge>
+            </Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>{props.data.qDes}</Card.Text>
 
-                  <Card.Text>
-                    請問室內貓每年該施打的疫苗有什麼？
-                    貓咪已經要三歲，前面疫苗都是每年打但是，疫苗真的每年都該打嗎？還是可以每2-3年再打？
-                    問了許多養貓的朋友都說貓咪都沒帶出去過所以他們也不少每年打
-                  </Card.Text>
+            <div className="text-right">
+              <Accordion.Toggle
+                as={Button}
+                variant="link"
+                eventKey="0"
+                // onClick={() => setOpen(!open)}
+              >
+                <h5>
+                  <Badge variant="primary">已回答</Badge>
+                </h5>
+              </Accordion.Toggle>
+            </div>
+          </Card.Body>
 
-                  <div className="text-right">
-                    <Accordion.Toggle
-                      as={Button}
-                      variant="link"
-                      eventKey="0"
-                      // onClick={() => setOpen(!open)}
-                    >
-                      <h5>
-                        <Badge variant="primary">已回答</Badge>
-                      </h5>
-                    </Accordion.Toggle>
-                  </div>
-                </Card.Body>
-
-                <Accordion.Collapse eventKey="0">
-                  <Card.Footer>
-                    <Card.Text>
-                      <p className="text-left">
-                        貓的注射疫苗臨床上以三合一（或五合一）以及狂犬病疫苗為主，近來也有開發出無佐劑的四合一（含白血病）疫苗，但單價較高一些。至於每年是否都該追加補強的問題，醫師只能告訴你，疫苗每次注射效力只能維持一年，至於是否會被疾病感染，就要看是否有傳染途徑的存在了。
-                      </p>
-                      <Image
-                        src="http://fakeimg.pl/50x50/20b2aa/EAE0D0/"
-                        roundedCircle
-                      />
-                    </Card.Text>
-                  </Card.Footer>
-                </Accordion.Collapse>
-              </Card>
-            </Accordion>
-            <br />
-            <Accordion>
-              <Card>
-                <Card.Header>
-                  <Card.Title as="h4">
-                    關於每年施打疫苗，是必須的嗎？
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>分類</Card.Subtitle>
-                  <div>
-                    <Card.Text>
-                      請問室內貓每年該施打的疫苗有什麼？
-                      貓咪已經要三歲，前面疫苗都是每年打但是，疫苗真的每年都該打嗎？還是可以每2-3年再打？
-                      問了許多養貓的朋友都說貓咪都沒帶出去過所以他們也不少每年打
-                    </Card.Text>
-                  </div>
-                  <div className="text-right">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                      <h5>
-                        <Badge variant="secondary">未回答</Badge>
-                      </h5>
-                    </Accordion.Toggle>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Accordion>
-          </Col>
-
-          <Col md={6}>
-            <Accordion>
-              <Card>
-                <Card.Header>
-                  <Card.Title as="h4">
-                    關於每年施打疫苗，是必須的嗎？
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                  <Card.Subtitle>分類</Card.Subtitle>
-                  <div>
-                    <Card.Text>
-                      請問室內貓每年該施打的疫苗有什麼？
-                      貓咪已經要三歲，前面疫苗都是每年打但是，疫苗真的每年都該打嗎？還是可以每2-3年再打？
-                      問了許多養貓的朋友都說貓咪都沒帶出去過所以他們也不少每年打
-                    </Card.Text>
-                  </div>
-                  <div className="text-right">
-                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
-                      <h5>
-                        <Badge variant="secondary">未回答</Badge>
-                      </h5>
-                    </Accordion.Toggle>
-                  </div>
-                </Card.Body>
-                {/* <Accordion.Collapse eventKey="0">
-                    <Card.Footer>
-                      <Card.Text>
-                        貓的注射疫苗臨床上以三合一（或五合一）以及狂犬病疫苗為主，近來也有開發出無佐劑的四合一（含白血病）疫苗，但單價較高一些。至於每年是否都該追加補強的問題，醫師只能告訴你，疫苗每次注射效力只能維持一年，至於是否會被疾病感染，就要看是否有傳染途徑的存在了。
-                      </Card.Text>
-                    </Card.Footer>
-                  </Accordion.Collapse> */}
-              </Card>
-            </Accordion>
-          </Col>
-        </Row>
-      </Container>
+          <Accordion.Collapse eventKey="0">
+            <Card.Footer>
+              <Card.Text>
+                <p className="text-left">{props.data.qAns}</p>
+                <Image
+                  src="http://fakeimg.pl/50x50/20b2aa/EAE0D0/"
+                  roundedCircle
+                />
+              </Card.Text>
+            </Card.Footer>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+      <br />
     </>
   )
 }
 
-export default QuestionArt
+export default withRouter(QuestionArt)
