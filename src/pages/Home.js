c4aacc0a38232256fd3d15a16bc51beee85bfa0c
lopthick <lopthick3@gmail.com>
2020-03-28 12:22:23 +0800

    登入測試

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,73 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Card } from 'react-bootstrap'
+import { Card, Modal, Button } from 'react-bootstrap'
+import ModalHeader from 'react-bootstrap/ModalHeader'
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import $ from 'jquery'
+import { IconContext } from 'react-icons'
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai'
+import { FaRegEdit } from 'react-icons/fa'
+//redux
+import { connect } from 'react-redux'
+//action
+import { bindActionCreators } from 'redux'
+import { getMemberData, getMemberDetail } from '../pages/member/actions/index'
 //引入自己的css
 import '../css/index/index.scss'
 import CarouselPage from '../components/index/carousel'
-import Header from '../components/Header'
+// import Header from '../components/Header'
 function Home(props) {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = function () {
+    handleClose2()
+    setShow(true)
+  }
+  const [show2, setShow2] = useState(false)
+  const handleClose2 = () => setShow2(false)
+  const handleShow2 = function () {
+    handleClose()
+    setShow2(true)
+  }
+
+  useEffect(() => {
+    // props.getMemberDetail(mId)
+    // props.getMemberData()
+    // window.location.reload()
+    //第一种只刷新一次
+    $('.show').click(function () {
+      console.log('show')
+      $('.show').addClass('active')
+      $('.hide').removeClass('active')
+      $('#exampleInputPassword1').attr('type', 'text')
+    })
+    $('.hide').click(function () {
+      console.log('hide')
+      $('.hide').addClass('active')
+      $('.show').removeClass('active')
+      $('#exampleInputPassword1').attr('type', 'password')
+    })
+  }, [])
   return (
     <>
-      <div className="container home_login home_hide">
-        <div className=" login-container">
+      <Button variant="primary" onClick={handleShow}>
+        登入跳出測試
+      </Button>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Body>
+          <ModalHeader></ModalHeader>
           <div className="login">
             <div
               className="alertBox alert alert-danger disappear"
               role="alert"
             ></div>
-            <img
-              src={require('../images/logo-dark.svg')}
-              alt="Background"
-              className="text-center"
-            />
+            <figure className="modal-logo text-center">
+              <img
+                src={require('../images/logo-dark.svg')}
+                alt="Background"
+                className="img-fluid"
+              />
+            </figure>
             <hr />
             <form>
               <div class="form-group">
@@ -35,46 +82,126 @@ function Home(props) {
               <div class="form-group">
                 <input
                   type="password"
-                  class="form-control"
+                  class="form-control position-relative"
                   id="exampleInputPassword1"
                   placeholder="密碼"
                 />
-                <img
-                  src={require('../images/member/hide_password.png')}
-                  alt="Background"
-                  className="show"
-                />
-                <img
-                  src={require('../images/member/show_hide_password.png')}
-                  alt="Background"
-                  className="hide active"
-                />
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'show' }}
+                >
+                  <AiOutlineEye />
+                </IconContext.Provider>
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'hide' }}
+                >
+                  <AiOutlineEyeInvisible />
+                </IconContext.Provider>
               </div>
               <Link class="form-group text-left">
                 <p>忘記密碼?</p>
               </Link>
-              <br />
-              <Link
-                type="submit"
-                class="btn btn-primary btn-block login-btn"
-                // to={'/member/'}
-              >
+              <Link type="submit" class="btn btn-theme btn-block login-btn">
                 登入
               </Link>
-              <div class="form-group d-flex justify-content-between register">
-                <div>
-                  <p>還沒有註冊帳號?</p>
-                </div>
-                <div>
-                  <Link class="" to="/register">
-                    <p>立即註冊→</p>
-                  </Link>
-                </div>
-              </div>
+              <Link
+                type="button"
+                class="btn btn-light btn-block login-btn"
+                onClick={(handleClose, handleShow2)}
+                // to="/register"
+              >
+                <span className="">立即註冊</span>
+              </Link>
             </form>
           </div>
-        </div>
-      </div>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+      <Modal show={show2} onHide={handleClose2} animation={false}>
+        <Modal.Body>
+          <div className="login">
+            <div
+              className="alertBox alert alert-danger disappear"
+              role="alert"
+            ></div>
+            <div className="d-flex justify-content-center">
+              <IconContext.Provider
+                value={{
+                  color: 'black',
+                  size: '5rem',
+                  className: 'signIn',
+                  style: { textAlign: 'center' },
+                }}
+              >
+                <FaRegEdit />
+              </IconContext.Provider>
+            </div>
+
+            <hr />
+            <form>
+              <div class="form-group">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="exampleInputAccount1"
+                  aria-describedby="accountHelp"
+                  placeholder="信箱"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control position-relative"
+                  id="exampleInputPassword1"
+                  placeholder="密碼"
+                />
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'show2' }}
+                >
+                  <AiOutlineEye />
+                </IconContext.Provider>
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'hide2' }}
+                >
+                  <AiOutlineEyeInvisible />
+                </IconContext.Provider>
+              </div>
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control position-relative"
+                  id="exampleInputPassword2"
+                  placeholder="再次確認密碼"
+                />
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'show3' }}
+                >
+                  <AiOutlineEye />
+                </IconContext.Provider>
+                <IconContext.Provider
+                  value={{ size: '1.5rem', className: 'hide3' }}
+                >
+                  <AiOutlineEyeInvisible />
+                </IconContext.Provider>
+              </div>
+              <Link class="form-group text-left" to="/register">
+                <p>前往完整註冊</p>
+              </Link>
+              <Link type="submit" class="btn btn-theme btn-block login-btn">
+                註冊
+              </Link>
+              <Link
+                type="button"
+                class="btn btn-light btn-block login-btn"
+                onClick={handleShow}
+              >
+                <span className="">返回登入頁</span>
+              </Link>
+            </form>
+          </div>
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+
       <div className="home">
         <CarouselPage />
         <div className="container">
@@ -273,7 +400,7 @@ function Home(props) {
                 <h2 className="text-center">PARK TIME</h2>
                 <div className="">
                   <button type="button" className="btn btn-outline-theme1">
-                    Discover Dog Collars
+                    立即前往選購
                   </button>
                 </div>
               </div>
@@ -286,7 +413,7 @@ function Home(props) {
                 <h2 className="text-center">LEAD ME</h2>
                 <div className="">
                   <button type="button" className="btn btn-outline-theme1">
-                    Discover Dog Collars
+                    立即前往選購
                   </button>
                 </div>
               </div>
@@ -301,7 +428,7 @@ function Home(props) {
               <div className="supply-info2">
                 <div className="">
                   <button type="button" className="btn btn-outline-theme1">
-                    Discover Dog Collars
+                    立即前往選購
                   </button>
                 </div>
               </div>
@@ -316,7 +443,7 @@ function Home(props) {
               <div className="supply-info2">
                 <div className="">
                   <button type="button" className="btn btn-outline-theme1">
-                    Discover Dog Collars
+                    立即前往選購
                   </button>
                 </div>
               </div>
@@ -331,7 +458,7 @@ function Home(props) {
               <div className="supply-info2">
                 <div className="">
                   <button type="button" className="btn btn-outline-theme1">
-                    Discover Dog Collars
+                    立即前往選購
                   </button>
                 </div>
               </div>
@@ -420,4 +547,10 @@ function Home(props) {
   )
 }
 
-export default Home
+const mapStateToProps = (store) => {
+  return { data: store.getMember, detail: store.getMemberDetail }
+}
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ getMemberData, getMemberDetail }, dispatch)
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
