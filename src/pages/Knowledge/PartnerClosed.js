52d3d66cbc4f092c23374b7ad27d1f34f6e06668
eloise <55773031+bananajumpjump@users.noreply.github.com>
2020-03-30 23:48:08 +0800

    update

--- src/pages/Knowledge/PartnerClosed.js
+++ src/pages/Knowledge/PartnerClosed.js
@@ -4,16 +4,15 @@ import { Card, Button, Row, Col, Image, Collapse, Badge } from 'react-bootstrap'
 import { IconContext } from 'react-icons'
 import { FaRegCalendarAlt, FaRegHeart, FaHeart } from 'react-icons/fa'
 import { FiClock } from 'react-icons/fi'
-import { MdDeleteForever } from 'react-icons/md'
+
 import { MdLocationOn } from 'react-icons/md'
 import $ from 'jquery'
-import { getMemberDetail } from '../member/actions/index'
+
 import { getPartnerPlusDetail } from './actions/index'
 //redux
 import { connect } from 'react-redux'
 //action
 import { bindActionCreators } from 'redux'
-import PartnerPlus from './PartnerPlus'
 
 function PartnerClosed(props) {
   const [open, setOpen] = useState(false)
@@ -58,52 +57,13 @@ function PartnerClosed(props) {
 
   return (
     <>
-<<<<<<< HEAD
       {props.plus &&
-        props.plus.map((value) => {
+        props.plus.map((value, index) => {
           return (
             <Card
+              key={index}
               className="partnercard m-3"
               // style={{ width: '33rem' }}
-=======
-      <Card className="partnercard" style={{ width: '33rem' }}>
-        <Card.Header className="justify-content-between d-flex">
-          <IconContext.Provider value={{ size: '1.2rem' }}>
-            <div>
-              <FaRegCalendarAlt />
-              <span className="carddate"> 活動日期：2020/05/07</span>
-            </div>
-            <div className="right">
-              <FiClock />
-              <span className="cardtime"> 時間：15:00-16:00</span>
-            </div>
-          </IconContext.Provider>
-        </Card.Header>
-        <Card.Img
-          variant="top"
-          src="http://fakeimg.pl/1300x400/eeeeee/EAE0D0/"
-        />
-        <Card.Body>
-          <Card.Title>
-            <Link to>基礎正向行為養成班／輕鬆打造快樂狗生活</Link>
-          </Card.Title>
-          <Card.Text>
-            針對毛孩教養時發生的日常行為問題，特別開設「正向行為禮貌養成班」，協助飼主引導毛孩學會基礎服從指令
-          </Card.Text>
-          <div className="text-right">
-            <Button
-              className="justify-right"
-              variant="outline-primary"
-              onClick={function () {
-                Swal.fire({
-                  title: '尚未開始報名',
-                  icon: 'warning',
-                  showCancelButton: false,
-                  confirmButtonColor: '#cea160',
-                  confirmButtonText: '確定',
-                })
-              }}
->>>>>>> 9e7b2b3b3982199bd6aafa821ef68cc768e13a66
             >
               <Card.Header className="justify-content-between d-flex">
                 <IconContext.Provider value={{ size: '1.2rem' }}>
